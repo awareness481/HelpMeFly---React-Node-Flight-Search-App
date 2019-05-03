0ac985fa05342f7a4757040acae6976991f0bc3c
Jesse Jafa <jesse.jafa@gmail.com>
2019-05-03 10:56:19 +0300

    implement date picker on form

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import Button from '@material-ui/core/Button';
 import FilledInput from '@material-ui/core/FilledInput';
 import Switch from '@material-ui/core/Switch';
 
+// DatePicker
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       start: '',
       destination: '',
-      oneway: false
+      oneway: false,
+      startDate: new Date(),
+      endDate: new Date().setDate(new Date().getDate() + 3)
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDate = this.handleDate.bind(this);
   }
 
   handleChange(event) {
@@ -26,6 +33,13 @@ class App extends React.Component {
     }
     
     console.log(event.target.name);
+  }
+
+  handleDate(date, event) {
+    console.log(event)
+    this.setState({
+      [event]: date
+    });
   }
 
   handleSubmit(event) {
@@ -42,6 +56,16 @@ class App extends React.Component {
           value={this.state.oneway}
         >
         </Switch>
+        <div className='dates'>
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={e => this.handleDate(e,'startDate')}
+          />
+          <DatePicker
+            selected={this.state.endDate}
+            onChange={e => this.handleDate(e,'endDate')}
+          />
+        </div>
         <div className='flightInput'>
           <FilledInput
             disableUnderline={true}
