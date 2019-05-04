6310aecc475a32de2b6e1461375c51d1fe21757d
Jesse Jafa <jesse.jafa@gmail.com>
2019-05-04 10:03:29 +0300

    improved colors and position of form & inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './_main.scss';
+import React from "react";
+import "./_main.scss";
 
-import Button from '@material-ui/core/Button';
-import FilledInput from '@material-ui/core/FilledInput';
-import Switch from '@material-ui/core/Switch';
+import Button from "@material-ui/core/Button";
+import FilledInput from "@material-ui/core/FilledInput";
+import Switch from "@material-ui/core/Switch";
 
 // DatePicker
 import DatePicker from "react-datepicker";
@@ -13,8 +13,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      start: '',
-      destination: '',
+      start: "",
+      destination: "",
       oneway: false,
       startDate: new Date(),
       endDate: new Date().setDate(new Date().getDate() + 3)
@@ -26,68 +26,75 @@ class App extends React.Component {
   }
 
   handleChange(event) {
-    if (event.target.name === 'oneway') {
-      this.setState( {[event.target.name] : !this.state.oneway})
+    if (event.target.name === "oneway") {
+      this.setState({ [event.target.name]: !this.state.oneway });
     } else {
-      this.setState({[event.target.name]: event.target.value});
+      this.setState({ [event.target.name]: event.target.value });
     }
-    
+
     console.log(event.target.name);
   }
 
   handleDate(date, event) {
-    console.log(event)
+    console.log(event);
     this.setState({
       [event]: date
     });
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.oneway);
+    alert("A name was submitted: " + this.state.oneway);
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className='flightForm'>
-        <Switch
-          onChange={this.handleChange}
-          name='oneway'
-          value={this.state.oneway}
-        >
-        </Switch>
-        <div className='dates'>
-          <DatePicker
-            selected={this.state.startDate}
-            onChange={e => this.handleDate(e,'startDate')}
-          />
-          <DatePicker
-            selected={this.state.endDate}
-            onChange={e => this.handleDate(e,'endDate')}
-          />
-        </div>
-        <div className='flightInput'>
-          <FilledInput
-            disableUnderline={true}
-            name='start'
-            onChange={this.handleChange}
-            required={true}
-            type='text'
-            value={this.state.start}
-          >
-          </FilledInput>
-          <FilledInput
-            disableUnderline={true}
-            name='destination'
-            onChange={this.handleChange}
-            required={true}
-            type='text'
-            value={this.state.destination}
-          >
-          </FilledInput>
-        </div>
-        <Button type='submit' value='Submit' className='submitButton'>Submit</Button>
-      </form>
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="flightForm">
+          <div className='switchButton'>
+            <Switch
+              onChange={this.handleChange}
+              name="oneway"
+              value={this.state.oneway}
+            />
+          </div>
+          <div className="dates">
+            <DatePicker
+              className='dates__start'
+              selected={this.state.startDate}
+              onChange={e => this.handleDate(e, "startDate")}
+            />
+            <DatePicker
+              className='dates__end'
+              selected={this.state.endDate}
+              onChange={e => this.handleDate(e, "endDate")}
+            />
+          </div>
+          <div className="flightInput">
+            <FilledInput
+              disableUnderline={true}
+              className='inputField inputField--left'
+              name="start"
+              onChange={this.handleChange}
+              required={true}
+              type="text"
+              value={this.state.start}
+            />
+            <FilledInput
+              disableUnderline={true}
+              className='inputField inputField--right'
+              name="destination"
+              onChange={this.handleChange}
+              required={true}
+              type="text"
+              value={this.state.destination}
+            />
+          </div>
+          <Button type="submit" value="Submit" className="submitButton">
+            Submit
+          </Button>
+        </form>
+      </div>
     );
   }
 }
